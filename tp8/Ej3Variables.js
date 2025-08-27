2502d78e3913ebd984571681484ef99d9e210daf
Joaquin Vallejos <joaquinnqn989@gmail.com>
2025-08-26 22:12:11 -0300

    feat: ejercicio 3

---
+++ tp8/Ej3Variables.js
@@ -0,0 +1,13 @@
+const nombre= "Joaquin";
+const apellido="Vallejos";
+let estado="Soltero";
+let Edad= 17;
+const Fecha= "19/03/2008";
+const DNI="48727619";
+
+document.write("Nombre: " + nombre + "<br>");
+document.write("Apellido: " + apellido + "<br>");
+document.write("Estado civil: " + estado + "<br>");
+document.write("Edad: " + Edad + "<br>");
+document.write("Fecha de nacimiento: " + Fecha + "<br>");
+document.write("DNI: " + DNI);
---
+++ tp8/Ej3VariablesArray.js
@@ -0,0 +1,5 @@
+
+let datos = ["Nombre: Joaquin", "Apellido: Vallejos", "Estado Civil: Soltero","Edad: 17", "Fecha de nacimiento: 19/03/08"
+,"DNI: 48727619"];
+for (let i=0; i<6; i++){
+    document.write (datos[i]+ "<br>")};
