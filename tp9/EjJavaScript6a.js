2e44250726e6a9d3edb3ecc6d15ad60faad6286d
Joaquin Vallejos <joaquinnqn989@gmail.com>
2025-09-03 19:57:19 -0300

    feat: ejercicio 6a

---
+++ tp9/EjJavaScript6a.js
@@ -0,0 +1,14 @@
+if (prompt("Contraseña?","")=="minombre"){
+
+    document.writeln("Nombre:Joaquin <br>");
+document.writeln("Apellido: Vallejos <br>");
+document.writeln("Estado civil: Soltero <br>");
+document.writeln("Edad:17 <br>");
+document.writeln("Fecha de nacimiento: 19/03/08 <br>");
+document.writeln("DNI: 48727619 <br>");
+
+} else {
+    
+    alert("MAAAAAL!!!!")
+
+}
